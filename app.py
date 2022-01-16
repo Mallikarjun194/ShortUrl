a5dcbd6c65d711dacf1d3e54ba4fa05ec8793124
mallikarjuna <mallikarjun.a1@ibm.com>
2022-01-16 16:23:40 +0530

    Refactored the app.py file and also handled file i/o error

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # #!/usr/bin/env python
 # # encoding: utf-8
+import os.path as path
 import json
 from flask import Flask, jsonify, request
 import random, string
@@ -19,64 +20,62 @@ def write_file(data):
 
 @app.route('/get', methods=['GET'])
 def get_short_url():
-    with open('shorturl.json', 'r') as f:
-        data = f.read()
-        file = json.loads(data)
-        record = json.loads(request.data)
-        for rec in file:
-            if rec['url'] == record['url']:
-                return jsonify({record['url']: rec[record['url']]})
-
-        else:
-            return jsonify({"Error_msg": "url not found, Please do a POST call"})
+    file = read_file()
+    data = json.loads(file)
+    param = request.args.get('url')
+    if param in data:
+        return jsonify({param: data[param]})
+    else:
+        return jsonify({"Error_msg": "url not found, Please do a POST call"})
 
 
 @app.route('/create', methods=['POST', 'PUT'])
 def create_record():
     record = json.loads(request.data)
     rand_alpha_num = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
-    # Creating a shorturl like myapp.com/<5-digit Alphanumeric>
     short_url = 'myapp.com/' + rand_alpha_num
-    if request.method == 'POST':
-        with open('shorturl.json', 'r') as f:
-            data = f.read()
-            if not data:
-                file = dict()
-                file[record['url']] = short_url
-            else:
-                file = json.loads(data)
-                if record['url'] in file.keys():
-                    return jsonify({'Error_msg': "url already present"})
-                else:
-                    file[record['url']] = short_url
-            with open('shorturl.json', 'w') as f:
-                f.write(json.dumps(file, indent=2))
-            return jsonify(file), 201
+    file1 = read_file()
+    if not file1:
+        data = dict()
+        data[record['url']] = short_url
     else:
-        with open('shorturl.json', 'r') as f:
-            data = f.read()
-            if not data:
-                file = dict()
-                file[record['url']] = short_url
-            else:
-                file = json.loads(data)
-                if record['url'] in file.keys():
-                    file[record['url']] = short_url
-                else:
-                    return jsonify({'Error_msg': "url not present, Please do a POST call"})
-            with open('shorturl.json', 'w') as f:
-                f.write(json.dumps(file, indent=2))
-            return jsonify(file), 201
+        data = json.loads(file1)
+        if record['url'] in data.keys():
+            return jsonify({'Error_msg': "url already shortened " + data[record['url']]})
+        else:
+            data[record['url']] = short_url
+    write_file(data)
+    return jsonify({"Success": "Your short url is "+data[record['url']]}), 201
+
+
+@app.route('/update', methods=['PUT'])
+def update_method():
+    record = json.loads(request.data)
+    rand_alpha_num = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+    short_url = 'myapp.com/' + rand_alpha_num
+    file1 = read_file()
+    data = json.loads(file1)
+    if record['url'] in data.keys():
+        data[record['url']] = short_url
+    else:
+        return jsonify({"Error_msg": "url not present, Please do a POST call"})
+    write_file(data)
+    return jsonify({"Success": "Your updated short url is " + data[record['url']]}), 201
 
 
 @app.route('/delete', methods=['DELETE'])
 def delete_record():
+    file1 = read_file()
+    data = json.loads(file1)
     record = json.loads(request.data)
-    if record['url'] in url.keys():
-        url.pop(record['url'])
-        return jsonify({"Success:": "url deleted successfully"})
+    if record['url'] in data:
+        data.pop(record['url'])
     else:
-        return jsonify({"Error_msg": "Requested url not found"})
+        return jsonify({"Error_msg": "url not found, Please do a POST call"})
+    write_file(data)
+    return jsonify({"Success:": "url deleted successfully "+ record['url']})
 
 
+if not path.exists("shorturl.json"):
+    write_file(dict())
 app.run()
